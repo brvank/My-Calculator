00ca7edb8dc61a610e67e22b47f01f4cfdc140aa
Ankit Kumar Maurya <mauryaankit482@gmail.com>
2021-06-29 17:02:27 +0530

    fragment in ConversionActivity

--- app/src/main/java/com/example/mycalculator/ConversionActivity.java
+++ app/src/main/java/com/example/mycalculator/ConversionActivity.java
@@ -1,14 +1,70 @@
 package com.example.mycalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.transition.FragmentTransitionSupport;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ConversionActivity extends AppCompatActivity {
 
+    TextView currency,distance,mass,volume;
+    LinearLayout fragmentView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversion);
+
+        currency = findViewById(R.id.currency);
+        distance = findViewById(R.id.distance);
+        mass = findViewById(R.id.mass);
+        volume = findViewById(R.id.volume);
+
+        fragmentView = findViewById(R.id.viewOfFragment);
+
+        currency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new CurrencyConversionFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(fragmentView.getId(),fragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+        distance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new DistanceConversionFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(fragmentView.getId(),fragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+        mass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new MassConversionFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(fragmentView.getId(),fragment);
+                fragmentTransaction.commit();
+            }
+        });
+
+        volume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new VolumeConversionFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(fragmentView.getId(),fragment);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
